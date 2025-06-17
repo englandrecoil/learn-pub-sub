46598caa062dcd741bdcc75d22d1f98f78681a85
englandrecoil <nik.tereshchenko.982@gmail.com>
2025-06-17 18:42:50 +0300

    add support for STOMP

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rbmqConnString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(rbmqConnString)
+	if err != nil {
+		log.Fatalf("Couldn't establish connection with RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Server successfully connected to RabbitMQ!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nClosing connection...")
 }
